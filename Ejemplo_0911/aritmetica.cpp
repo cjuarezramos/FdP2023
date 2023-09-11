b0a57b15aeb5fd30996079e23ab9a9cd1e555953
Carlos Juarez <cjuarez@uca.edu.sv>
2023-09-11 15:03:25 -0600

    ejemplos artimetica y comparacion

---
+++ Ejemplo_0911/aritmetica.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int a = 2;
+    int b = 5;
+    int c = -4;
+    int d = 0;
+    float e = 0;
+    int f;
+
+    cout << a + b << "\n" << a - b << "\n";
+    cout << (e + b)/a <<"\n" << (d + b)/a <<"\n";
+    
+    return 0;
+}
---
+++ Ejemplo_0911/comparaciones.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int d = 6;
+
+    cout << (a == b) << "\n";
+    cout << (a != b) << "\n";
+    cout << (b < d) << "\n";
+    cout << (a > d) << "\n";
+    cout << (b <= d) << "\n";
+
+    return 0;
+}
