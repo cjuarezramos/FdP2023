30a261a1004afe699833e0785067b6c6a15c0577
Carlos Juarez <cjuarez@uca.edu.sv>
2023-09-12 11:31:13 -0600

    ejemplo booleano

---
+++ Ejemplo_0911/booleano.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool a = false;
+    bool b = 0;
+    bool c = true;
+    bool d = 6;
+    bool e = -8;
+    cout << a << "\n"<< b << "\n" ;
+    cout << c << "\n"<< d << "\n"<< e << "\n";
+    return 0;
+}
